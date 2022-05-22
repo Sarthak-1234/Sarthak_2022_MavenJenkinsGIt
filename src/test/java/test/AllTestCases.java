895bc88500685506fb37aeb726f693a2d739745d
Sarthak <sarthak.mzn@gmail.com>
2022-05-22 22:29:24 +0530

    Initial Commit

---
+++ src/test/java/test/AllTestCases.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class AllTestCases {
+
+	@Test
+	public void SanityTestA() {
+		System.out.println("Sanity Testcase A");
+	}
+	
+	@Test
+	public void SanityTestB() {
+		System.out.println("Sanity Testcase B");
+	}
+	
+	@Test
+	public void RegressionTestA() {
+		System.out.println("Regression Testcase A");
+	}
+	
+	@Test
+	public void RegressionTestB() {
+		System.out.println("Regression Testcase B");
+	}
+	
+}
